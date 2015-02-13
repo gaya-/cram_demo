aeeed2e2346f47f59eab897d4c1798c2d1c6e872
Jan Winkler <winkler@cs.uni-bremen.de>
2015-02-13 08:00:57 +0100

    Import table setting symbols from cram_task_knowledge

--- cram_tablesetting_demo/src/package.lisp
+++ cram_tablesetting_demo/src/package.lisp
@@ -54,6 +54,7 @@
   (:import-from :cram-language def-cram-function def-top-level-cram-function)
   (:import-from :cram-reasoning def-fact-group <-)
   (:import-from :cram-roslisp-common *tf2*)
+  (:import-from :cram-task-knowledge table-setting-object situation?)
   (:desig-properties #:grasp-type #:push #:top-slide-down
                      #:scene #:dinnerplate #:shape #:name #:on
                      #:pancakemix #:flat #:box #:muesli
